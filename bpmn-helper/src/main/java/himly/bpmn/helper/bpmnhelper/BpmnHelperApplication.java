1b52e0a3edd3ae2fec26372fe35aeaa38626d6e8
zheng <z1399956473@gmail.com>
2019-03-18 13:52:21 +0800

    Add bpmn-helper module

---
+++ bpmn-helper/src/main/java/himly/bpmn/helper/bpmnhelper/BpmnHelperApplication.java
@@ -0,0 +1,13 @@
+package himly.bpmn.helper.bpmnhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmnHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmnHelperApplication.class, args);
+    }
+
+}
